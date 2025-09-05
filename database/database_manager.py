4f68a6d08847a42fae3a87352e83fc73152078de
Mostov <vladimircev327@gmail.com>
2025-09-05 20:45:00 +0400

    Доп функции менеджера

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
-from utils import InvalidCSVError
+import csv
+from utils import InvalidCSVError, InvalidAddUserError
 from core import PasswordHasher
 
 
@@ -8,15 +9,26 @@ class DataBaseManager:
     def __init__(self):
         self.users = pd.DataFrame()
         self.ph = PasswordHasher
+        self.file_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), '..', 'system_file', 'users.csv')
+        )
         self.__read_users()
 
     def __read_users(self):
         try:
-            file_path = os.path.join(os.path.dirname(__file__), '..', 'system_file', 'users.csv')
-            file_path = os.path.abspath(file_path)
-            self.users = pd.read_csv(file_path, encoding='UTF-8')
+            self.users = pd.read_csv(self.file_path, encoding='UTF-8')
         except Exception as e:
             print(InvalidCSVError(str(e)))
+
+    def __get_max_id(self):
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                ids = [int(row["id"]) for row in reader]
+                max_id = max(ids) if ids else 0
+        except FileNotFoundError:
+            max_id = 0
+        return max_id
 
     def get_users(self):
         return self.users
@@ -30,6 +42,42 @@ class DataBaseManager:
         return None
 
     def check_user_availability(self, login):
-        row = self.users.loc[self.users["name"] == login]
-        if row:
-            return True
+        return not self.users.loc[self.users["name"] == login].empty
+
+    def check_email_availability(self, email):
+        return not self.users.loc[self.users["email"] == email].empty
+
+    def check_phone_availability(self, phone):
+        cleaned_phone = phone.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
+        return not self.users.loc[self.users["phone"] == cleaned_phone].empty
+
+    def add_new_user(self, login: str, email: str, phone: str, password: str):
+        cleaned_phone = (phone.replace("(", "")
+                         .replace(")", "")
+                         .replace("-", "")
+                         .replace(" ", ""))
+        hash_password = self.ph.hash_password(password)
+
+        # Данные для записи
+        new_id = self.__get_max_id() + 1
+        try:
+            self.users.loc[len(self.users)] = {
+                'id': new_id,
+                'name': login,
+                'hash': hash_password,
+                'email': email,
+                'phone': cleaned_phone,
+                'role': 'buyer',
+                'balance': 0
+            }
+
+            self.users.to_csv(self.file_path, index=False, encoding='UTF-8')
+        except Exception as e:
+            print(InvalidAddUserError(str(e)))
+
+
+if __name__ == "__main__":
+    database_manager = DataBaseManager()
+    print(database_manager.check_user_availability('32133'))
+
+    # database_manager.add_new_user("1", "1", "1", "1")
